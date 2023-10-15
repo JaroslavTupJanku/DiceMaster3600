26f63bcfca054dfbd4651f4f491f0b535ae5b47f
Jaroslav Janku <j_janku@utb.cz>
2023-10-15 16:48:30 +0200

    Database Created

--- DiceMaster3600.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace DiceMaster3600.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ DiceMaster3600.Data/Entitites/UniversityEntity.cs
@@ -0,0 +1,17 @@
+﻿using DiceMaster3600.Core.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceMaster3600.Data.Entitites
+{
+    public class UniversityEntity
+    {
+        public int Id { get; set; }
+
+        public UniversityType Name { get; set; } 
+        public ICollection<FacultyEntity> Faculties { get; set; } = null!;
+    }
+}
