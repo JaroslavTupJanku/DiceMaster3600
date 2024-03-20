50851fccebc4154e8eecc5aaf0d6f8157fc4e759
Jaroslav Janku <j_janku@utb.cz>
2024-03-20 01:04:50 +0100

    Added EntryForm

---
+++ DiceMaster3600/View/Converters/BooleanToBrushConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DiceMaster3600.View.Converters
+{
+    public class BooleanToBrushConverter : IValueConverter
+    {
+        public Brush? TrueBrush { get; set; }
+        public Brush? FalseBrush { get; set; }
+
+        public object? Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool v && v ? TrueBrush : FalseBrush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as Brush == TrueBrush;
+        }
+    }
+}
--- DiceMaster3600/obj/Debug/net6.0-windows/View/HomeView.g.i.cs
+++ DiceMaster3600/obj/Debug/net6.0-windows/View/HomeView.g.i.cs
@@ -52,7 +52,7 @@ namespace DiceMaster3600.View {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/DiceMaster3600;V1.0.0.0;component/view/homeview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/DiceMaster3600;component/view/homeview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\View\HomeView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
